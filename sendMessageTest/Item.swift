a34f71757caf701f5c79e73c36e0476b5a48ebd4
Ewill25 <116986519+Ewill25@users.noreply.github.com>
2024-01-26 15:08:17 -0800

    Initial Commit

---
+++ sendMessageTest/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  sendMessageTest
+//
+//  Created by Ethan Willform on 1/26/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
